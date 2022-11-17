f569f86413e9fccc6c97c454ab400a3452f65c3b
Jonahcas <casafam@bellsouth.net>
2022-11-17 16:26:20 -0500

    Redid url.py

--- django-env/mysite/polls/urls.py
+++ django-env/mysite/polls/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-
+from django.contrib import admin
+from django.urls import include, path
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
 ]
 
+urlpatterns = [
+    path('polls/', include('polls.urls')),
+    path('admin/', admin.site.urls),
+]
