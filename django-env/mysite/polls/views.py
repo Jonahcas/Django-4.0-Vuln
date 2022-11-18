e5c7ddc7d10aca00df4c7f29baeb030981882f99
Jonahcas <casafam@bellsouth.net>
2022-11-18 12:01:37 -0500

    Reverted changes
    Going to create a second app besides polls

--- django-env/mysite/polls/views.py
+++ django-env/mysite/polls/views.py
@@ -33,33 +33,3 @@ def vote(request, question_id):
         selected_choice.votes += 1
         selected_choice.save()
         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
-#----------------------------------------------------------------------------------
-#----------------------------------------------------------------------------------
-from django.http.response import JsonResponse
-from datetime import datetime
-from django.db.models.functions import Extract, Trunc
-from django.db.models import DateTimeField
-from polls.models import Experiment
-from django.core import serializers
-
-# /extract/?lookup_name=xxx
-def polls_extract(request):
-    payload = request.GET.get('lookup_name')
-    start = datetime(2015, 6, 15)
-    end = datetime(2015, 7, 2)
-    Experiment.objects.create(
-        start_datetime=start, start_date=start.date(),
-        end_datetime=end, end_date=end.date())
-    experiments = Experiment.objects.filter(start_datetime__year=Extract('end_datetime', payload))
-    return JsonResponse({"res": serializers.serialize("json", experiments)})
-
-# /trunc/?kind=xxx
-def polls_trunc(request):
-    payload = request.GET.get('kind')
-    start = datetime(2015, 6, 15)
-    end = datetime(2015, 7, 2)
-    Experiment.objects.create(
-        start_datetime=start, start_date=start.date(),
-        end_datetime=end, end_date=end.date())
-    experiments = Experiment.objects.filter(start_datetime__date=Trunc('start_datetime', payload))
-    return JsonResponse({"res": serializers.serialize("json", experiments)})
